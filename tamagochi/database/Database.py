22c30526b985871c0ff55fb1286f810d372c17ac
tandera@notemail.com <email@email.com>
2023-05-20 02:41:22 -0300

    Added JWT creation + login/register

--- tamagochi/database/Database.py
+++ tamagochi/database/Database.py
@@ -10,11 +10,41 @@ class SQLite3Manager:
         self.connection = None
 
     def getConnection(self):
-        if not self.connection:
+        if not self.connection or self._isClosed(self.connection):
             self.connection = sqlite3.connect(self.database)
 
         return self.connection
+    
+    def _isClosed(self, conn: sqlite3.Connection) -> bool:
+        try:
+            conn.execute('')
+        except sqlite3.ProgrammingError:
+            return True
+        
+        return False
+    
+    def query(self, sql: str, params: Tuple[str, str] = (), type: str = "select"):
+        """
+        Query the database. 
+        Arguments:
+         sql: SQL statement, such as 'SELECT email, password FROM Users WHERE id = ?'
+         params: tuple of parameters to be inserted in the sql 
+         type: type of the operation ("select", "change")
+        """
 
-    def select(self, select_query: str, params: Tuple[str, str] = ()):
-        return self.getConnection().cursor().execute(select_query, params).fetchall()
+        conn = self.getConnection()
+        cursor = conn.cursor()
+        cursor.execute(sql, params) 
+        
+        if type == "select":
+            result = cursor.fetchall()
+
+        if type == "change":
+            result = []
+            conn.commit()
+
+        cursor.close()
+        conn.close()
+        return result
+
 
--- tamagochi/routes/parent.py
+++ tamagochi/routes/parent.py
@@ -11,15 +11,39 @@ jwt = JWTManager()
 def on_load(state):
     jwt.init_app(state.app)
 
+def checkFields(data_obj, *fields):
+    for value in fields:
+        if data_obj.get(value) is None:
+            return False
+
+    return True
+
 @parent.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
+    
+    if checkFields(data, 'password', 'email'):
+        return {'error': 'missing fields'}, 400
+
     email, password = data.get('email'), data.get('password')
-    entries_found = db.select('SELECT COUNT(*) FROM pai WHERE email = ? AND senha = ?', (email, password))[0][0]
+    entries_found = db.query('SELECT COUNT(*) FROM pai WHERE email = ? AND senha = ?', (email, password))[0][0]
 
     if entries_found == 1:
         access_token = create_access_token(identity=email)
         return {'access_token': access_token}, 200
     else:
         return {'error': 'Invalid email or password'}, 401
-    
+
+@parent.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    name, surname = data.get('name'), data.get('surname')
+    email, password = data.get('email'), data.get('password')
+    gender = data.get('gender')
+
+    db.query('INSERT INTO pai (nome, sobrenome, email, senha, sexo) VALUES (?, ?, ?, ?, ?)',
+             (name, surname, email, password, gender), type="change")
+
+    return { "status": "registered" }
+
+
